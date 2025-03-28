274cd1b12eb39a0669c94f8b7debbc1f0455468a
LukaMiloshev <lukamiloshev@yahoo.com>
2025-03-28 14:07:14 +0100

    Add files via upload

---
+++ Дополнителна домашна/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chart from "./components/Chart";
+
+function App() {
+    return (
+        <div>
+            <h1>Financial Data Dashboard</h1>
+            <Chart />
+        </div>
+    );
+}
+
+export default App;
